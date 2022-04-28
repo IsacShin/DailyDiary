c6da6fee9eb343edfee431a6830a3dddc44b38e4
isac <isac9305@gmail.com>
2022-04-28 17:27:19 +0900

    기록 페이지 추가

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:dailyapp/views/widget/feedbox.dart';
 import 'package:dailyapp/views/write.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
 
 void main() {
   runApp(const MyApp());
@@ -38,6 +39,7 @@ class _MyHomePageState extends State<MyHomePage> {
   int currentIndex = 0;
   DateTime dateTime = DateTime.now();
 
+  List<Feed> allFeeds = [];
   List<Feed> feeds = [];
 
   @override
@@ -48,7 +50,14 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   void getHistories() async {
-    feeds = await dbHelper.queryAllFeed();
+    allFeeds = await dbHelper.queryAllFeed();
+    setState(() {});
+  }
+
+  void getDateHistory() async {
+    int _d = Utils.getFormatTime(dateTime);
+    feeds = await dbHelper.queryFeedByDate(_d);
+    print(feeds.length);
     setState(() {});
   }
 
@@ -60,7 +69,7 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(""),
       ),
       body: getPage(),
-      floatingActionButton: FloatingActionButton(
+      floatingActionButton: currentIndex != 0 ? Container() : FloatingActionButton(
         onPressed: () async {
           await Navigator.push(
             context,
@@ -107,6 +116,8 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget getPage() {
     if(currentIndex == 0) { // Home
       return getHomeWidget();
+    }else if(currentIndex == 1) { // calendar
+      return getCalendarWidget();
     }
 
     return Container();
@@ -115,12 +126,42 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget getHomeWidget() {
     // return FeedBoxWidget();
 
-    return feeds.isEmpty ? Container(
+    return allFeeds.isEmpty ? Container(
       child: const Center(
         child: Text("작성된 피드가 없습니다.\n 오늘의 피드를 작성해주세요.", textAlign: TextAlign.center, style: TextStyle(fontSize: 18),),
       ),
     ) : ListView(
       padding: EdgeInsets.fromLTRB(0, 16, 0, 45),
+      children: List.generate(allFeeds.length, (idx){
+        return InkWell(
+          child:FeedBoxWidget(f: allFeeds[idx],),
+          onTap: () async{
+            await Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => FeedWritePage(f:allFeeds[idx])),
+            );
+
+            getHistories();
+
+          },
+        );
+      }),
+    );
+  }
+
+  Widget getHistoryWidget() {
+    // return FeedBoxWidget();
+
+    return feeds.isEmpty ? Container(
+      height: 300,
+      child: const Center(
+        child: Text("작성된 피드가 없습니다.", textAlign: TextAlign.center, style: TextStyle(fontSize: 18),),
+      ),
+    ) : ListView(
+      scrollDirection: Axis.vertical,
+      shrinkWrap: true,
+      physics: const NeverScrollableScrollPhysics(), // listview > listview in scroll disable
+      padding: const EdgeInsets.fromLTRB(0, 16, 0, 45),
       children: List.generate(feeds.length, (idx){
         return InkWell(
           child:FeedBoxWidget(f: feeds[idx],),
@@ -129,11 +170,47 @@ class _MyHomePageState extends State<MyHomePage> {
               context,
               MaterialPageRoute(builder: (context) => FeedWritePage(f:feeds[idx])),
             );
-
             getHistories();
+            getDateHistory();
           },
         );
       }),
+    );
+  }
+
+  Widget getCalendarWidget() {
+    return Container(
+      child: ListView.builder(itemBuilder: (ctx, idx){
+        if(idx == 0) {
+          return Container(
+              child: TableCalendar(
+                firstDay: dateTime.subtract(Duration(days: 365*10 + 2)),
+                lastDay: dateTime.add(Duration(days: 365*10 + 2)),
+                focusedDay: dateTime,
+                selectedDayPredicate: (day) {
+                  return isSameDay(dateTime, day);
+                },
+                onDaySelected: (selectedDay, focusedDay) {
+                  dateTime = selectedDay;
+                  getDateHistory();
+                },
+                headerStyle: HeaderStyle(
+                    titleCentered: true
+                ),
+                calendarFormat: CalendarFormat.month,
+                availableCalendarFormats: {
+                  CalendarFormat.month: ""
+                },
+              )
+          );
+        }else if(idx == 1) {
+          return getHistoryWidget();
+        }
+
+        return Container();
+      },
+        itemCount: 2,
+      ),
     );
   }
 }
